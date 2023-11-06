2a20ecdcd99d3408599818744b91e52e2c4fa2a4
Bei Jia <bei.jia@dcmail.ca>
2023-11-06 09:20:33 -0500

    change css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # change the root 
   root 'companies#index'
   resources :companies
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
